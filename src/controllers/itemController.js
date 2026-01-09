489cfebc2c0af91d24445b90f1f22469b699d877
cool-noodle <ronilaster1@gmail.com>
2026-01-09 16:15:44 +0200

    safety changes

--- src/controllers/itemController.js
+++ src/controllers/itemController.js
@@ -6,30 +6,19 @@ const getAllItems = async (req, res) => {
 }
 
 const getSearchedItems = async (req, res) => {
-    const { q } = req.query.q || req.query.query || '';
+    const { q } = req.query;
 
     try {
-        const filter = q ? { name: { $regex: q, $options: 'i' } } : {};
-        const items = await Item.find(filter);
+        let items;
+        if (q) {
+            items = await Item.find({ name: { $regex: q, $options: 'i' } })
+        }
 
         res.status(200).json(items);
 
     } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: "Server error" });
+        res.status(500).json({ message: "No items found" })
     }
-
-    // try {
-    //     let items;
-    //     if (q) {
-    //         items = await Item.find({ name: { $regex: q, $options: 'i' } })
-    //     }
-
-    //     res.status(200).json(items);
-
-    // } catch (error) {
-    //     res.status(500).json({ message: "No items found" })
-    // }
 }
 
 const getSingleItem = async (req, res) => {
