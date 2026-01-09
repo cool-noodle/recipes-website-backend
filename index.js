bc84f33d1b15195382469492e58f9eccadf3c1aa
cool-noodle <ronilaster1@gmail.com>
2026-01-09 14:48:55 +0200

    safety changes

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express')
 require('dotenv').config();
 const mongoose = require('mongoose');
-const app = express()
 const cors = require('cors');
+
+const app = express();
 const port = process.env.PORT || 5000;
 
 //mpl704mv
@@ -12,15 +13,17 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 
-async function main() {
-    await mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.log(err));
 
-    app.get('/', (req, res) => {
-        res.send('Veggify Recipe App Server is Running')
-    })
-}
 
-main().then(() => console.log('mongodb connected succeffuly!')).catch(err => console.log(err));
+app.get('/', (req, res) => {
+    res.send('Recipe App Server is Running')
+})
+
+
+// main().then(() => console.log('mongodb connected succeffuly!')).catch(err => console.log(err));
 
 // routes
 const ItemRoutes = require('./src/routes/itemRoute');
@@ -30,6 +33,6 @@ app.use('/api', ItemRoutes);
 app.use('/api', CategoryRoutes)
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Server running on port ${port}`)
 })
 
